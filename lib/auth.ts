d231f52a8ee727cf8708279f0e6eb7612328e4c6
Rishikesh <rishikesh.kanabar@gmail.com>
2023-11-03 19:21:06 -0400

    feat(auth): add mock user as a credential provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,15 +2,32 @@ import { AuthOptions } from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials';
 import prisma from '@/lib/db'
 
+const previewProviders = [
+  CredentialsProvider({
+    // @ts-ignore comment
+    async authorize() {
+      return {
+        id: 1,
+        name: 'Mock User',
+        email: 'mock.user@example.com',
+        image: 'https://i.pravatar.cc/150?u=jsmith@example.com',
+      };
+    },
+  }),
+]
+
+const productionProviders = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  }),
+]
+
 export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
+  providers: process.env.VERCEL_ENV === 'preview' ? previewProviders : productionProviders,
   secret: process.env.NEXTAUTH_SECRET as string,
   adapter: PrismaAdapter(prisma),
 }
