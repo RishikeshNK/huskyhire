638ed3f5bc5609d758dc19f0c6aeae81d44e3a6a
Rishi Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-11-03 14:30:55 -0400

    Add dark mode `<ThemeProvider>` using `shadcn/ui` (#19)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 
+import Container from "@/components/container";
+import ModeToggle from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-2">
-      <h1 className="text-3xl">Husky Hire</h1>
-      <Button onClick={() => signIn('google', { callbackUrl: '/profile' })}>Sign in</Button>
-    </div>
+    <Container>
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-2xl font-bold">Husky Hire</h1>
+        <ModeToggle />
+      </div>
+    </Container>
   );
 }
