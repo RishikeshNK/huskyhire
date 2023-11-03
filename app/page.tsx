7002886bc3569508bfc2b951197fe68cbcc26fc5
Rishikesh <rishikesh.kanabar@gmail.com>
2023-11-03 13:37:45 -0400

    feat(ui): add simple routes for signing in and out

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
 export default function Home() {
-  return <div>Husky Hire</div>;
+  return (
+    <div className="flex flex-col items-center justify-center space-y-2">
+      <h1 className="text-3xl">Husky Hire</h1>
+      <Button onClick={() => signIn('google', { callbackUrl: '/profile' })}>Sign in</Button>
+    </div>
+  );
 }
